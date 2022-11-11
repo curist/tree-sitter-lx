bdb6ad69c517d51f1359e9ddbe10d53d704a118c
curist <curist.cyc@gmail.com>
2022-11-11 10:55:20 +0800

    add field name for hashmap braces

--- vim/queries/lx/highlights.scm
+++ vim/queries/lx/highlights.scm
@@ -22,6 +22,7 @@
  "(" ")"
  "[" "]"
  "{" "}"
+ "{" "}"
 ] @punctuation.bracket
 
 [ "if" "else" ] @conditional
@@ -35,6 +36,7 @@
 
 [ ";" "," "." ] @punctuation.delimiter 
 
+(hashmap) @punctuation.bracket
 (keyvalue key: (identifier) @field)
 
 (identifier) @variable
