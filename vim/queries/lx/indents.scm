c3a68f03f6189264ca4bc57a903d115b22293633
curist <curist.cyc@gmail.com>
2022-11-30 22:46:20 +0800

    using tree-sitter indents.scm

---
+++ vim/queries/lx/indents.scm
@@ -0,0 +1,37 @@
+[
+ (array)
+ (hashmap)
+ (argument_list)
+ (parameter_list)
+ (variable_declaration)
+ (function_declaration)
+ (block)
+ (assignment)
+ (unary_expression)
+ (binary_expression)
+] @indent
+
+(if (then_clause) @indent)
+
+[
+ ")"
+ "}"
+ "]"
+] @indent_end
+
+[
+ "else"
+ ")"
+ "}"
+ "]"
+] @branch
+
+
+(shell_bang) @zero_indent
+
+(string) @ignore
+
+(comment) @auto
+
+
+
