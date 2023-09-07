5f193934c4ea325ad870d77ba1b56ee041fc3198
curist <curist.cyc@gmail.com>
2023-09-07 12:26:40 +0800

    update global fns
    add first, last

--- vim/queries/lx/highlights.scm
+++ vim/queries/lx/highlights.scm
@@ -21,7 +21,7 @@
        "foldr" "len" "print" "println" "groan" "groanln" "random"
        "getline" "read" "tonumber" "tolower" "toupper"
        "take" "drop" "join" "startsWith" "endsWith"
-       "system" "exec"
+       "system" "exec" "first" "last"
        ))
 
 [
